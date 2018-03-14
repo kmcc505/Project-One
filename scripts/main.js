a63a5410799cbc937602c4b978f60b360971ba18
Abdullah <abdullah.alwaheb@gmail.com>
2018-03-13 20:43:32 -0500

    google places api

---
+++ scripts/main.js
@@ -0,0 +1,47 @@
+var map;
+var infowindow;
+var service;
+
+
+function initMap() {
+    var myCoords = { lat: -95.3698, lng: 29.7604 };
+    var pyrmont = {lat: -33.867, lng: 151.195};
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: myCoords,
+        zoom: 14
+    });
+
+    infowindow = new google.maps.InfoWindow();
+
+    var request = {
+        location: myCoords,
+        radius: '1000',
+        query: 'restaurant'
+    };
+
+    service = new google.maps.places.PlacesService(map);
+    service.textSearch(request, callback);
+
+
+    function callback(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+                createMarker(results[i]);
+            }
+        }
+    }
+
+    function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+            map: map,
+            position: place.geometry.location
+        });
+
+        google.maps.event.addListener(marker, 'click', function () {
+            infowindow.setContent(place.name);
+            infowindow.open(map, this);
+        });
+    }
+}
